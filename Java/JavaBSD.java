6b3d19c1858c0c52a6c0e0665360c2b6d8cf8829
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-06-04 21:39:10 -0500

    Add files via upload

---
+++ Java/JavaBSD.java
@@ -0,0 +1,26 @@
+/*
+Kevin Nguyen
+Basic Java program to compute the sum of differences between sets of bits.
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaBSD"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class JavaShell {
+	//Global Declarations
+
+	//main driver
+	public static void main(String[] args) {
+		//local Declarations
+		System.out.println("HelloWorld");
+	}
+
+	//function definitions below	
+}
